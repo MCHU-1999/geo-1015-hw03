4f79cdb21e3358e1f0c2585c10f8ffc89d9d7ae5
Lars van Blokland <standardcomputation.com>
2025-01-06 16:57:41 +0100

    small change to Hough Transform

--- code/houghtransform.py
+++ code/houghtransform.py
@@ -1,4 +1,23 @@
 import numpy as np
 
-if __name__ == "__main__":
-    print("Yep, it's empty.")
+
+def detect(lazfile, params, viz=False):
+    """
+    !!! TO BE COMPLETED !!!
+    !!! You are free to subdivide the functionality of this function into several functions !!!
+
+    Function that detects all the planes in the input LAZ file.
+
+    Inputs:
+      lazfile: a laspy input file
+      params: a dictionary with all the parameters necessary for the algorithm
+      viz: whether the visualiser (rerun, or polyscope) should be displaying results or not
+
+    Output:
+      - a NumPy array Nx4; each point has x-y-z-segmentid
+    """
+    segment_ids = np.random.randint(low=0, high=10, size=lazfile.header.point_count)
+    pts = np.vstack((lazfile.x, lazfile.y, lazfile.z, segment_ids)).transpose()
+    print(pts)
+    print(pts)
+    return pts
